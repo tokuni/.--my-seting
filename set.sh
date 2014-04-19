f616f736c560f2e29cd9697fc76a7dc8d7ae2854
aaaaaaaa <2cba3fed@gmail.com>
2014-04-19 09:17:56 +0900

    Update set.sh
    fix

--- set.sh
+++ set.sh
@@ -3,9 +3,9 @@
 mkdir -p ~/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 
-a = "if has('vim_starting') \r   set runtimepath+=~/.vim/bundle/neobundle.vim/ \r endif \r call neobundle#rc(expand(\'~/.vim/bundle/\')) \r NeoBundleFetch \'Shougo/neobundle.vim\' \r filetype plugin on \r NeoBundleCheck"
+a="if has('vim_starting') \r   set runtimepath+=~/.vim/bundle/neobundle.vim/ \r endif \r call neobundle#rc(expand(\'~/.vim/bundle/\')) \r NeoBundleFetch \'Shougo/neobundle.vim\' \r filetype plugin on \r NeoBundleCheck"
  
 
-echo a >> ~/.vimrc
-a = "NeoBundle \'Shougo/unite.vim\' \r NeoBundle \'Shougo/unite.vim\' \r NeoBundle \'Shougo/neocomplcache.vim\' \r NeoBundle \'slimv.vim\' "
-echo a >> ~/.vimrc
+echo $a >> ~/.vimrc
+a="NeoBundle \'Shougo/unite.vim\' \r NeoBundle \'Shougo/unite.vim\' \r NeoBundle \'Shougo/neocomplcache.vim\' \r NeoBundle \'slimv.vim\' "
+echo $a >> ~/.vimrc
